7b70b151d4bcf46e2112f8a01281fd08f1e14229
naveenshiraguppi <naveen.shiraguppi@gmail.com>
2020-12-22 11:40:24 +1100

    Adding search functionality.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
+import { useState, useEffect} from 'react';
 import logo from './logo.svg';
+import icon from './ICON.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React!!!
-        </a>
+        <Search/>
+        <div>
+        </div>
       </header>
     </div>
   );
 }
 
+function Results({ records }) {
+  return (
+    <ul>
+      {
+        <li className="label">{JSON.stringify(records, undefined, 2)}</li>
+      }
+    </ul>
+  );
+}
+function SearchText() {
+  const [val, setVal] = useState("");
+  const [data, setData] = useState(null);
+  useEffect(() => {
+    setData(null);
+    fetch('http://localhost:8080/api/search/' + val)
+    .then(res => res.json())
+    .then(setData)
+    .catch(console.error);
+  }, [val]);
+  
+
+  return (
+    <>
+        <input id="searchtext" name="searchtext" type="input"
+             className="search" placeholder="Search by lot code or description...."
+             value = {val}
+             onChange={ e => setVal(e.target.value)}/>
+       <Results records={data}/>
+    </>
+  );
+}
+function Search() {
+    return ( 
+        <>
+          <div>
+            
+            <label className="label">Wine Search</label>  <img src={icon} className="inline" alt="icon"/>
+            </div>
+            <SearchText/>
+            
+        </>
+      );
+}
 export default App;
